d400fd2ad12b8fd1150227c82823ea6d6b880440
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2021-12-04 08:56:52 +0800

    added print in new line and concatenation

--- day-1-printing-start.py
+++ day-1-printing-start.py
@@ -1,1 +1,12 @@
 print("Hello World!")
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+
+# be careful when using single ('') or double quotes ("")
+print("print('what to print')")
+
+# printing strings in a new line using \n
+print("Hello World!\nHello World!\nHello World!")
+
+# String concatenation using +
+print("Hello" + " " + "Khairi!")
