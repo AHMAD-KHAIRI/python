a98a5a0e9d15876347ac94e08ad2c1f79ebd6a70
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2021-12-04 10:13:33 +0800

    first commit added input function

---
+++ day-2-input-function.py
@@ -0,0 +1,8 @@
+# for detailed explanation refer to Day-2-input-function-Notebook-Sheet.pdf
+
+# input() will get the user to type in response in the console. Then, the text will replace the input() function
+#input("What is your name?")
+
+# input() will get user to input
+# then print() will print out "Hello " and the user input
+print("Hello " + input("What is your name?\n"))
