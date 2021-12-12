3a1d4e048356632d97aada00bc3a1dbb4c1eb6ba
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2021-12-12 10:09:03 +0800

    first commit

---
+++ day-4-data-types.py
@@ -0,0 +1,36 @@
+# Data Types in Python
+
+# String
+# print("Hello")
+
+# The method to call out/ pull out an element from a string is call subscripting [n]
+print("Hello"[0])  # The first character in a string always starts from 0 --> H
+print("Hello"[1])  # Pull out the 2nd character --> e
+print("Hello"[2])  # Pull out the 3rd character --> l
+print("Hello"[3])  # Pull out the 4th character --> l
+print("Hello"[4])  # Pull out the 5th character --> o
+
+# Integer
+123456789
+
+# math calculations
+print(123 + 345)    # + is the addition operator
+print(123 - 1)      # - is the subtraction operator
+print(123 * 2)      # * is the multiplication operator
+print(123 / 2)      # / is the division operator
+print(123 % 2)      # % is the remainder operator
+
+# for Large integers, we use '_' to divide groups of digits in the number literal so that it is easier to read
+print(123_456_789)  # 123,456,789
+
+
+# Floating Point Number (Float)
+Pi = 3.14159
+print(Pi)
+
+
+# Boolean - they are either True or False and must start with uppercase
+
+True
+# true
+False
