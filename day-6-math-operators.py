84fd45f4dd20bbfd6b63a49cb0320a330193e95b
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-08-25 09:31:06 +0800

    added math operators and some math exercises

---
+++ day-6-math-operators.py
@@ -0,0 +1,30 @@
+3 + 5
+7 - 4
+3 * 2
+6 / 3
+# division outputs a float data type
+print(type(6 / 3))
+2 ** 3
+
+# PEMDAS / BODMAS rule in mathematics
+# executed from Left to Right (l2R)
+# () Parentheses / Bracket
+# ** Exponents / Order 
+# * Multiplication and / Division
+# + Addition 
+# - Subtraction
+
+# example:
+print(3 * 3 + 3 / 3 - 3)
+# first order: 3 * 3 = 9 
+# second order: 3 / 3 = 1
+# third order: 9 + 1 = 10
+# fourth order: 10 - 3 = 7 --> output
+
+# challenge: change the result of the math operation above to 3.0
+print(3 * (3 + 3) / 3 - 3)
+# (3 + 3) = 6
+# 3 * 6 = 18
+# 18 / 3 = 6
+# 6 - 3 = 3
+
