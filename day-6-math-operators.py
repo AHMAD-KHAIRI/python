c4139e8d2777de379617968ad301456bca0ec71e
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-08-27 10:35:08 +0800

    added the bmi calculation coding exercise with solution

--- day-6-math-operators.py
+++ day-6-math-operators.py
@@ -28,3 +28,49 @@ print(3 * (3 + 3) / 3 - 3)
 # 18 / 3 = 6
 # 6 - 3 = 3
 
+# Day 2 / Exercise 2 - BMI Calculator
+# Coded on 27.08.2022
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+# print(type(height), type(weight)) --> type str
+
+# from the user input, convert height and weight from str to float data type
+height_conv_to_float = float(height)
+
+weight_conv_to_float = float(weight)
+
+# calculate the height squared and save it in a variable
+height_squared = height_conv_to_float ** 2
+
+# print(type(height_conv_to_float), type(weight_conv_to_float))
+
+# here are multiple ways to do the mathematics calculation to get the bmi result:
+# bmi_result_float = weight_conv_to_float / (height_conv_to_float * height_conv_to_float)
+# bmi_result_float = weight_conv_to_float / (height_conv_to_float ** 2)
+bmi_result_float = weight_conv_to_float / (height_squared)
+
+# print(result_float)
+
+# print out the bmi calculation formula and the results
+print(weight, "÷ (", height, " x ", height, ") = ", bmi_result_float)
+
+# convert the bmi result from float to int data type
+bmi_result_int = int(bmi_result_float)
+
+# display the bmi results in int data type
+print(bmi_result_int)
+
+
+# solution from udemy trainer
+bmi = int(weight) / float(height) ** 2
+bmi_as_int = int(bmi)
+print(bmi_as_int)
+
+
+
+
