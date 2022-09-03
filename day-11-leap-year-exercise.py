20a9df85a6c516b91d501219f7e52ee813334b78
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-09-03 14:57:20 +0800

    added exercise 3 - leap year

---
+++ day-11-leap-year-exercise.py
@@ -0,0 +1,8 @@
+# Exercise 3 - Leap Year
+# Coded on 03.09.2022
+
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
