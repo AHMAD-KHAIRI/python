46e84bfb83cc3c0f9e2ba90c6a3912a97b69508b
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-09-24 11:07:40 +0800

    added examples of defining and calling python functions

---
+++ day-22-define-and-call-functions.py
@@ -0,0 +1,30 @@
+# 59. Defining and calling Python functions
+# Coded on 24.09.2022
+
+# print() function
+print("Hello")
+
+# len() function
+num_char = len("Hello")
+print(num_char)
+
+# How to make our own funtion
+# def name_of_function(input):
+    # content of the function
+
+def my_function():
+    print("Hello AK!")
+
+# Calling a function
+# name_of_function()
+my_function()
+
+# Defining functions
+# def my_function():
+    # Do this
+    # Then do this
+    # Finally do this
+
+# Calling functions
+# my_function()
+
