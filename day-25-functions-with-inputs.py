4e5248c278eae08c35670384dc8f98e4e3ea008d
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-10-05 09:00:49 +0800

    Added example of functions with inputs

---
+++ day-25-functions-with-inputs.py
@@ -0,0 +1,33 @@
+# 80. Day 8 
+# Coded on 05.10.2022
+
+# Build a cipher program "Caesar Cipher"
+
+# Review:
+# Create a function called greet()
+# Write 2 print statements inside the function
+# Call the greet() function and run your code
+
+def greet():
+    print("Good Morning AK!")
+    print("Happy Programming!")
+    print("Don't Give Up!")
+
+greet()
+
+# functions with inputs
+# def my_function(something): where something is called the Parameter-the name of data that is being passed in
+    # Do this with something
+    # Then do this
+    # Finally do this
+
+# my_function(123) where 123 is called the Argument-the actual value of the data
+
+
+# Function that allows for input
+def greet_with_name(name):
+    print(f"Good Morning {name}!")
+    print("Happy Programming!")
+    print("Don't Give Up!")
+
+greet_with_name("Khairi")
