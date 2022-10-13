8083ad8cbfb0ad50b2bf13d6586f8edb06e4c8bd
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-10-13 09:22:52 +0800

    Added solution to step 2 caesar cipher coding challenge - decrypt message

--- day-26-caesar-cipher.py
+++ day-26-caesar-cipher.py
@@ -13,8 +13,6 @@ num_of_char_in_text = len(text)
 
 text_to_list = list(text)
 
-    
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
@@ -24,27 +22,41 @@ def encrypt(plain_text, shift_amount):
         new_letter = alphabet[new_position]
         cipher_text += new_letter
     print(f"The encoded text is {cipher_text}")
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello" 
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+# 86. Caesar Cipher Part 2 - Decryption
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt(plain_text=text, shift_amount=shift)
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(cipher_text, shift_amount):
+    decipher_text = ""
+    for letter in cipher_text:
+        # find the letter using index() method. The index() method finds the first occurrence of the specified value
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        decipher_text += new_letter
+    print(f"The decoded text is {decipher_text}")
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == 'encode':
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == 'decode':
+    decrypt(cipher_text=text, shift_amount=shift)
 
 # How to compare between two strings and find the position
-test_string = "abcdefghijklmnopqrstuvwxyz"
-msg = input("Type something: ").lower()
+# test_string = "abcdefghijklmnopqrstuvwxyz"
+# msg = input("Type something: ").lower()
 
-def find_alphabet_in(message):
-    for letter in message:
-        position = test_string.index(letter)
-        print(position)
+# def find_alphabet_in(message):
+#     for letter in message:
+#         position = test_string.index(letter)
+#         print(position)
 
-find_alphabet_in(message=msg)
+# find_alphabet_in(message=msg)
+
