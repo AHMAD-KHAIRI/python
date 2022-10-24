404ee7bf2f96a51451ffadd0c7a0ca9ba7e351ed
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-10-24 11:22:53 +0800

    Added final solution to caesar cipher coding exercise such as importing a logo, adding a while loop and if else statement to restart and end the program

--- day-26-caesar-cipher.py
+++ day-26-caesar-cipher.py
@@ -1,25 +1,47 @@
 # 85. Caesar Cipher Part 1
 # Coded on 10.08.2022
+from day27caesarCipherArt import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     if cipher_direction == "decode":
         shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        # decode: 5 * -1 = -5
-        # encode: 12 + (-5) = 7
-        end_text += alphabet[new_position]
+    for char in start_text:
+        # if the user enters a space/number/symbol, we want the code to keep/maintain
+        # solution: use if statement to check for the characters
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            # decode: 5 * -1 = -5
+            # encode: 12 + (-5) = 7
+            end_text += alphabet[new_position]
+        # if the character is not in the alphabet list, just add the character back (space/number/symbol)
+        else:
+            end_text += char
     print(f"The {cipher_direction}d text is {end_text}")
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+print(logo)
+
+# How to implement continuous loop aka the user still wants to encode/decode a message
+# solution: use while loop and create a variable to store the condition
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    # if the user enters a shift number too large (greater than the number of letters in the alphabet list), we will get an index error. 
+    # Solution: use % to get the remainder
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    # asks the user if they still want to encode/decode or exit
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if result == 'no':
+        should_continue = False
+        print("Goodbye!")
 
 # def encrypt(plain_text, shift_amount):
 #     cipher_text = ""
