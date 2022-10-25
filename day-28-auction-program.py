2cf64b6c199ee16199367aeb6e89d526971307dd
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-10-25 23:43:33 +0800

    first commit

---
+++ day-28-auction-program.py
@@ -0,0 +1,11 @@
+# 95. The Secret Auction Program Instructions and Flow Chart
+# Coded on 25.10.2022
+
+from day28AuctionProgramArt import logo
+from os import system
+
+print(logo)
+
+#HINT: You can clear the output in the console using below:
+# system('cls')
+
