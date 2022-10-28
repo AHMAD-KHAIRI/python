295a1a5e69d7ffe3c3e05a5d852dc20c54bf3dd0
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-10-29 01:02:42 +0800

    added solution to auction program

--- day-28-auction-program.py
+++ day-28-auction-program.py
@@ -22,21 +22,39 @@ print(logo)
 # },
 
 # first create an empty dictionary in list
-bidding_data = []
+# bidding_data = []
+bids = {}
+bidding_finish = False
 
-def add_new_bidder(bidder_name, bid_amount):
-    # first create an empty dictionary
-    new_bidder = {}
-    # secondly, assign the key with the function parameters
-    new_bidder["name"] = bidder_name
-    new_bidder["bid"] = bid_amount
-    # finally, to add an item to an existing list, we can use append()
-    bidding_data.append(new_bidder)
+# def add_new_bidder(bidder_name, bid_amount):
+#     # first create an empty dictionary
+#     new_bidder = {}
+#     # secondly, assign the key with the function parameters
+#     new_bidder["name"] = bidder_name
+#     new_bidder["bid"] = bid_amount
+#     # finally, to add an item to an existing list, we can use append()
+#     bidding_data.append(new_bidder)
 
-name = input("What is your name?: ")
-amount = int(input("What's your bid?: $"))
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
 
-add_new_bidder(bidder_name=name, bid_amount=amount)
-
-print(bidding_data)
-# print(bidding_data["bid"])
+while not bidding_finish:
+    name = input("What is your name?: ")
+    price = int(input("What's your bid?: $"))
+    bids[name] = price
+    # add_new_bidder(bidder_name=name, bid_amount=amount)
+    bid_continue = input("Are there any other bidders? Type 'yes' or 'no':\n")
+    # if no, exit the loop and determine the winner
+    if bid_continue == "no": 
+        bidding_finish = True
+        find_highest_bidder(bids)
+    # if yes, clear the screen and repeat
+    elif bid_continue == "yes":
+        system('cls')  
