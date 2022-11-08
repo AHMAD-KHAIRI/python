062d4691284a25a66b15396b69b92188a66fe2c1
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-11-08 08:33:37 +0800

    update day-29

--- day-29-functions-with-outputs.py
+++ day-29-functions-with-outputs.py
@@ -31,8 +31,11 @@
 # How to format a string where words start with an uppercase character and the remaining characters are lowercase
 # use the method title()
 def format_name(f_name, l_name):
-    # print(f_name.title())
-    # print(l_name.title())
+    # Docstrings (""" """) are a documenting feature for multi-line comment that provides an overview of the function description
+    """ Take a first and last name and format it to return the title case version of the name."""
+    # multiple return values:
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
     # print(f"{formatted_f_name} {formatted_l_name}")
@@ -41,9 +44,39 @@ def format_name(f_name, l_name):
 
 # print(format_name("ahmad khairi", "bin hamzah"))
 # or we can do this:
-formatted_string = format_name("ahmad khairi", "bin hamzah")
+formatted_string = format_name(input("What is your first name? "), input("What is your last name? "))
+# formatted_string = format_name("ahmad khairi", "bin hamzah")
 print(formatted_string)
 
 # Example of other functions which returns outputs
-str_length = len(formatted_string)
-print(str_length)
+# str_length = len(formatted_string)
+# print(str_length)
+
+
+# 101. Days in Month coding challenge
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+def days_in_month(year, month):
+    if month > 12 or month < 1:
+        return "Invalid month."
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month - 1]
+  
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
