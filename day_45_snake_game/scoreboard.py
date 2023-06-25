25e5f375215f385afb390d9869c2a9152db11c6c
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2023-06-25 23:30:27 +0800

    updated snake game to open, read and write high score data

--- day_45_snake_game/scoreboard.py
+++ day_45_snake_game/scoreboard.py
@@ -7,7 +7,14 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        # open and read a file
+        # relative path using ../ doesn't work
+        # with open("/../../../../snake_game_data.txt") as data:
+        with open("snake_game_data.txt") as data:
+            # converted from str to int
+            self.high_score = int(data.read())
+        # removed and replaced with open()
+        # self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -25,6 +32,9 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            # open and write a file
+            with open("snake_game_data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
     
