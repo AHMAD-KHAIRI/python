1ba9a2c427d5a924ddb227a598353219846c579d
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2023-07-10 00:29:21 +0800

    added automated birthday wisher project

---
+++ day_55_automated_birthday_wisher/day-55-smtp-datetime.py
@@ -0,0 +1,75 @@
+import smtplib, random
+import datetime as dt
+
+my_email = "ak.test.smtp.python@gmail.com"
+# Gmail app: python_birthday_wisher
+my_app_password = "mghdwtncpaiajeeb"
+recipient_email = "khairihamzah85@gmail.com"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+print(weekday)
+
+# if it is Monday
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_app_password)
+        connection.sendmail(
+            from_addr= my_email, 
+            to_addrs=my_email, 
+            msg=f"Subject: Monday Motivation\n\n{quote}\nThis email is sent using Python SMTP"
+            )
+
+
+'''
+# SMTP: Simple Mail Transfer Protocol
+import smtplib
+
+# Gmail: smtp.gmail.com
+# Hotmail: smtp.live.com
+# Outlook: outlook.office365.com
+# Yahoo: smtp.mail.yahoo.com
+
+
+my_email = "ak.test.smtp.python@gmail.com"
+# Gmail app: python_birthday_wisher
+my_app_password = "mghdwtncpaiajeeb"
+
+
+# Google SMTP Port
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=my_app_password)
+    connection.sendmail(
+        from_addr= my_email, 
+        to_addrs="aktestsmtppython@yahoo.com", 
+        msg="Subject:Hello!\n\nThis is a test email using Python SMTP"
+        )
+
+# Using this way forces us to close the connection at the end:
+# connection = smtplib.SMTP("smtp.gmail.com", port=587)
+# connection.starttls()
+# connection.login(user=my_email, password=my_app_password)
+# connection.sendmail(from_addr= my_email, to_addrs="aktestsmtppython@yahoo.com", msg="Subject:Hello!\n\nThis is a test email using Python SMTP")
+# # connection.sendmail(from_addr= my_email, to_addrs="aktestsmtppython@yahoo.com", msg="Hello!")
+# connection.close()
+
+
+# datetime module
+import datetime as dt
+
+# how we can get the current date, time, year, month, etc
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_the_week = now.weekday()
+print(day_of_the_week)
+
+date_of_birth = dt.datetime(year=1985, month=9, day=27)
+'''
---
+++ day_55_automated_birthday_wisher/main.py
@@ -0,0 +1,43 @@
+import smtplib, pandas, random
+from datetime import datetime
+
+my_email = "ak.test.smtp.python@gmail.com"
+# Gmail app: python_birthday_wisher
+my_app_password = "mghdwtncpaiajeeb"
+
+# 1. Check if today matches a birthday in birthdays.csv
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+# 2. Import birthdays.csv as dataframe
+data = pandas.read_csv("birthdays.csv")
+
+# 3. Create a dictionary from birthdays.csv that is formatted like this:
+# birthdays_dict = {
+    # (birthday_month, birthday_day): data_row
+    # }
+# use dictionary comprehension keyword: new_dict = {new_key:new_value for (index, data_row) in data.iterrows()}
+# birthdays_dict = {(data_row["month"], (data_row["day"])): data_row for (index, data_row) in data.iterrows()}
+birthdays_dict = {(data_row.month, (data_row.day)): data_row for (index, data_row) in data.iterrows()}
+
+# 4. Compare if today's month/day tuple matches one of the keys in the birthdays dictionary
+if today_tuple in birthdays_dict:
+    birthday_name = birthdays_dict[today_tuple]
+    # 5. If there is a match, pick a random letter from letter templates folder
+    file_path = f"./letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        # replace the placeholder with the birthday name
+        contents = contents.replace("[NAME]", birthday_name["name"])
+
+# 5. Send the letter generated using SMTP
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_app_password)
+        connection.sendmail(
+            from_addr= my_email, 
+            to_addrs=birthday_name["email"], 
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+            )      
+# YourName,your_own@email.com,today_year,today_month,today_day
+# name,email,year,month,day
