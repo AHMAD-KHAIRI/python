1a56e15a04fc5e5e381c52490ffee0c76ed11f77
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2023-08-10 00:13:39 +0800

    added some more python decorators and higher lower game with flask

--- day_73_web_dev_with_flask/hello.py
+++ day_73_web_dev_with_flask/hello.py
@@ -4,12 +4,41 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    # render html with inline css
+    return "<h1 style='text-align: center'>Hello, World!</h1> \
+    <p>This is a paragraph.</p> \
+    <img src='https://media.giphy.com/media/sFTWiBKYYWKVa/giphy.gif'>"
+    # return "<p>Hello, World!</p>"
+
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
 
 @app.route("/bye") # assigning a decorator function
+@make_bold
+@make_emphasis
+@make_underlined
 def say_bye():
     return "Bye"
+
+@app.route("/username/<name>/<int:number>") # creating variable paths and converting path to a specified data type
+# @app.route("/username/<name>")
+def greet(name, number):
+    return f"Hello {name}! You are {number} years old!"
+
 
 # To run flask, type in the console: python -m flask --app hello run
 
@@ -19,5 +48,6 @@ def say_bye():
 
 # Instead of typing the command in the console, we can just include the code below and run normally:
 if __name__ == "__main__":
-    app.run()
+    # app.run()
+    app.run(debug=True) # run the flask app in debug mode to auto-reload
 
