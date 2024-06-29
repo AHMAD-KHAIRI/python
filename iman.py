0545751f1c9b19bbe0d8e664c42540bcb286ed3d
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2024-06-29 08:47:51 +0200

    add everything

--- iman.py
+++ iman.py
@@ -15,3 +15,12 @@ print(f"Your favorite video is {favorite_video}.")
 
 favorite_food = input("What is your favorite food?\n")
 print(f"Your favorite food is {favorite_food}.")
+
+favorite_youtuber = input("Who is your favorite YouTuber?\n")
+print(f"Your favorite YouTuber is {favorite_youtuber}.")
+
+favorite_game = input("What is your favorite game to play?\n")
+print(f"Your favorite game is {favorite_game}.")
+
+favorite_number = input("What is your favorite number?\n")
+print(f"Your favorite number is {favorite_number}.")
