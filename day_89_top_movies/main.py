88d234bc3b745371684e62c169bc273194bb0a90
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2025-07-03 11:07:04 +0800

    just commit

--- day_89_top_movies/main.py
+++ day_89_top_movies/main.py
@@ -22,7 +22,7 @@ This will install the packages from requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 tmdb_api_key = os.environ.get("TMDB_API_KEY")
 # tmdb_api_read_access_token = os.environ.get("TMDB_API_READ_ACCESS_TOKEN")
 Bootstrap5(app)
---
+++ iman_seconds.py
@@ -0,0 +1,16 @@
+Iman = 7
+Mummy = 39
+Daddy = 40
+Atuk1 = 75
+Opah1 = 79
+
+seconds_in_a_minute = 60
+seconds_in_an_hour = seconds_in_a_minute * 60
+seconds_in_a_day = seconds_in_an_hour * 24
+seconds_in_a_year = seconds_in_a_day * 365
+
+print(f"Iman has lived for: {Iman * seconds_in_a_year:,} seconds")
+print(f"Mummy has lived for: {Mummy * seconds_in_a_year:,} seconds")
+print(f"Daddy has lived for: {Daddy * seconds_in_a_year:,} seconds")
+print(f"Atuk1 has lived for: {Atuk1 * seconds_in_a_year:,} seconds")
+print(f"Opah1 has lived for: {Opah1 * seconds_in_a_year:,} seconds")
